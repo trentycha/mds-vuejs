b119f9d2af4838b91052ff7b0d9562aa2a8b3e81
Adrien Russo <adrien@appro-auto.com>
2025-10-14 16:15:06 +0200

    feat: initialize Vue project with Vite
    - Add package.json with dependencies for Vue and Vite
    - Create Vite logo SVG file
    - Set up main application structure in App.vue
    - Implement main.js to bootstrap the Vue app with router
    - Add HomeView component for the home page
    - Configure Vue Router in router.js
    - Create initial style.css file
    - Set up Vite configuration in vite.config.js

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <RouterLink to="/">Home</RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped></style>
